85ad6853ff24f60be6982db5d34280a2ad8fccdc
whitestrannik <strannik.work@outlook.com>
2017-11-26 22:49:25 +0200

    - Added new AsyncLazy<> class

---
+++ Strannon.ConcurrentExtension.Test/AsyncLazyFixture.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Strannon.ConcurrentTestExtension;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Strannon.ConcurrentExtension.Test
+{
+    [TestClass]
+    public sealed class AsyncLazyFixture
+    {
+        [TestMethod]
+        public void WhenLazyValueIsNotReady_WeShouldWait()
+        {
+            var mre = new ManualResetEvent(false);
+            var sut = new AsyncLazy<int>(
+                () =>
+                {
+                    mre.WaitOne();
+                    return 42;
+                });
+
+            ConcurrentAssert.EnsureThatTaskIsNeverCompleted(sut.Value);
+        }
+
+        [TestMethod]
+        public void WhenLazyValueIsReady_WeShouldGetIt()
+        {
+            var mre = new ManualResetEvent(false);
+            var sut = new AsyncLazy<int>(
+                () =>
+                {
+                    mre.WaitOne();
+                    return 42;
+                });
+
+            mre.Set();
+            ConcurrentAssert.EnsureThatTaskIsCompleted(sut.Value);
+        }
+
+        [TestMethod]
+        public void WhenLazyValueCreatedWithTaskFactoryIsNotReady_WeShouldWait()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var sut = new AsyncLazy<int>(() => tcs.Task);
+
+            ConcurrentAssert.EnsureThatTaskIsNeverCompleted(sut.Value);
+        }
+
+        [TestMethod]
+        public void WhenLazyValueCreatedWithTaskFactoryIsReady_WeShouldGetIt()
+        {
+            var tcs = new TaskCompletionSource<int>();
+            var sut = new AsyncLazy<int>(() => tcs.Task);
+
+            tcs.SetResult(42);
+            ConcurrentAssert.EnsureThatTaskIsCompleted(sut.Value);
+        }
+    }
+}
---
+++ Strannon.ConcurrentExtension/AsyncLazy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Strannon.ConcurrentExtension
+{
+    public sealed class AsyncLazy<T> : Lazy<Task<T>>
+    {
+        public AsyncLazy(Func<T> factory) : base(() => Task.Run(factory))
+        { }
+
+        public AsyncLazy(Func<Task<T>> factory) : base(() => Task.Run(factory))
+        { }
+    }
+}
